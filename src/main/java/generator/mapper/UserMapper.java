0317958efb5a7653e842e988bbd8a161f3f549d5
chenzhihao <chenzhihao@itcast.cn>
2024-07-30 04:47:39 +0800

    1.  后端初始化
          1. 准备环境（MySQL 之类的）验证 MySQL 是否安装成功 - 连接一下 ✔
          2. 初始化后端项目，引入框架（整合框架）✔
    
    2. 数据库设计

---
+++ src/main/java/generator/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package generator.mapper;
+
+import generator.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 86137
+* @description 针对表【user(用户表)】的数据库操作Mapper
+* @createDate 2024-07-30 04:11:16
+* @Entity generator.domain.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
---
+++ src/main/java/generator/service/UserService.java
@@ -0,0 +1,13 @@
+package generator.service;
+
+import generator.domain.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author 86137
+* @description 针对表【user(用户表)】的数据库操作Service
+* @createDate 2024-07-30 04:11:16
+*/
+public interface UserService extends IService<User> {
+
+}
