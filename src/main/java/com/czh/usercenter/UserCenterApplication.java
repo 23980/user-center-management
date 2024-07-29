fabeed13a047515de5a5c186b3eaf4d85c2a9dfe
chenzhihao <chenzhihao@itcast.cn>
2024-07-30 04:14:56 +0800

    1.  后端初始化
          1. 准备环境（MySQL 之类的）验证 MySQL 是否安装成功 - 连接一下 ✔
          2. 初始化后端项目，引入框架（整合框架）✔
    
    2. 数据库设计

---
+++ src/main/java/com/czh/usercenter/UserCenterApplication.java
@@ -0,0 +1,15 @@
+package com.czh.usercenter;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.czh.usercenter.mapper")
+public class UserCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/czh/usercenter/UserCenterApplicationTests.java
@@ -0,0 +1,27 @@
+package com.czh.usercenter;
+
+import com.baomidou.mybatisplus.core.toolkit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+class UserCenterApplicationTests {
+
+
+    @Test
+    void contextLoads() {
+    }
+
+
+    @Test
+    public void testSelect() {
+
+    }
+
+}
